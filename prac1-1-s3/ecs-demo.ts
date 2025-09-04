1c7f9658f56b0e876dd763e9b01b098870f07368
cloudnative-prac901 <cloud.native.practice.25@gmail.com>
2025-09-05 02:06:02 +0900

    Update ecs-demo.ts
    add space

--- prac1-1-s3/ecs-demo.ts
+++ prac1-1-s3/ecs-demo.ts
@@ -4,6 +4,7 @@ import { App } from 'aws-cdk-lib';
 import { NetStack } from '../lib/net-stack';
 import { VpceStack } from '../lib/vpce-stack';
 import { AlbStack } from '../lib/alb-stack';    // ★追加
+
 const app = new App();
 const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
