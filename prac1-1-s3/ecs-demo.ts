829fd298ce1d1ad19a06676a7da6632edf8ea0b8
cloudnative-prac901 <cloud.native.practice.25@gmail.com>
2025-09-04 18:24:43 +0900

    Create ecs-demo.ts
    first commit

---
+++ prac1-1-s3/ecs-demo.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import { App } from 'aws-cdk-lib';
+import { NetStack } from '../lib/net-stack';
+import { VpceStack } from '../lib/vpce-stack';
+import { AlbStack } from '../lib/alb-stack';    // ★追加
+const app = new App();
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region : process.env.CDK_DEFAULT_REGION,
+};
+
+// VPC / Subnets / SecurityGroups
+const net = new NetStack(app, 'NetStack', { env });
+
+// VPC Endpoints
+new VpceStack(app, 'VpceStack', {
+  env,
+  vpc: net.vpc,
+  vpceSg: net.vpceSg,
+  ecsSg: net.ecsSg,
+});
+
+// ALB / WAF
+const alb = new AlbStack(app, 'AlbStack', {
+  env,
+  vpc: net.vpc,
+  albSg: net.albSg,
+});
