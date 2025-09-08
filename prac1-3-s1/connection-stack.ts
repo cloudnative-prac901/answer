9054d6a7723fa96ec5f95bf49cac0b850a88add8
cloudnative-prac901 <cloud.native.practice.25@gmail.com>
2025-09-08 17:51:01 +0900

    Update connection-stack.ts
    add comment

--- prac1-3-s1/connection-stack.ts
+++ prac1-3-s1/connection-stack.ts
@@ -1,23 +1,26 @@
+// 1. インポート
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codestar from 'aws-cdk-lib/aws-codestarconnections';
 
+// 2. クラス宣言、他スタックに公開するプロパティ
 export class ConnectionStack extends cdk.Stack {
-  public readonly connectionArn: string;
+  public readonly connectionArn: string;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  // 3. スタック初期化
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    // 4. コネクション作成
+    const conn = new codestar.CfnConnection(this, 'GitHubConnection', {
+      connectionName: 'CustomerInfoGitHub',
+      providerType: 'GitHub',
+    });
+    this.connectionArn = conn.attrConnectionArn;
 
-    const conn = new codestar.CfnConnection(this, 'GitHubConnection', {
-      connectionName: 'CustomerInfoGitHub',
-      providerType: 'GitHub',
-    });
-
-    this.connectionArn = conn.attrConnectionArn;
-
-    new cdk.CfnOutput(this, 'GitHubConnectionArn', {
-      value: this.connectionArn,
-      exportName: 'GitHubConnectionArn',
-    });
-  }
+    // 5. 出力
+    new cdk.CfnOutput(this, 'GitHubConnectionArn', {
+      value: this.connectionArn,
+      exportName: 'GitHubConnectionArn',
+    });
+  }
 }
