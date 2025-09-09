9e0bf1b1e75e40825c6f4f5dd4bf15b3de19882d
cloudnative-prac901 <cloud.native.practice.25@gmail.com>
2025-09-10 04:09:26 +0900

    Create ecs-demo.ts
    first commit

---
+++ prac1-4-s1/ecs-demo.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import { App } from 'aws-cdk-lib';
+import { NetStack } from '../lib/net-stack';
+import { VpceStack } from '../lib/vpce-stack';
+import { AlbStack } from '../lib/alb-stack';
+import { EcrStack } from '../lib/ecr-stack';
+import { RdsStack } from '../lib/rds-stack';
+import { EcsStack } from '../lib/ecs-stack';
+import { ConnectionStack } from '../lib/connection-stack';  // ★追加
+
+const app = new App();
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region : process.env.CDK_DEFAULT_REGION,
+};
+
+// VPC / Subnets / SecurityGroups
+const net = new NetStack(app, 'NetStack', { env });
+
+// VPC Endpoints
+new VpceStack(app, 'VpceStack', {
+  env,
+  vpc: net.vpc,
+  vpceSg: net.vpceSg,
+  ecsSg: net.ecsSg,
+});
+
+// ALB / WAF
+const alb = new AlbStack(app, 'AlbStack', {
+  env,
+  vpc: net.vpc,
+  albSg: net.albSg,
+});
+
+// ECR
+const ecr = new EcrStack(app, 'EcrStack', { env });
+
+// RDS
+const rds = new RdsStack(app, 'RdsStack', {
+  env,
+  vpc: net.vpc,
+  dbSg: net.dbSg,
+});
+
+// ECS / Fargate
+new EcsStack(app, 'EcsStack', {
+  env,
+  vpc        : net.vpc,
+  ecsSg      : net.ecsSg,
+  repo       : ecr.repository,
+  targetGroup: alb.targetGroup,
+});
+
+// CodeConnection  ★追加
+const conn = new ConnectionStack(app, 'ConnectionStack', { env });
