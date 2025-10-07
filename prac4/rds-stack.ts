5784a0c43825418972989f6b017df3c7d15fde9c
cloudnative-prac901 <cloud.native.practice.25@gmail.com>
2025-10-07 16:34:34 +0900

    Create rds-stack.ts
    first commit

---
+++ prac4/rds-stack.ts
@@ -0,0 +1,78 @@
+// 1. インポート
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+
+// 2. インタフェース定義
+export interface RdsStackProps extends cdk.StackProps {
+  vpc: ec2.IVpc;
+  dbSg: ec2.ISecurityGroup;
+}
+
+// 3. スタック初期化
+export class RdsStack extends cdk.Stack {
+  public readonly dbInstance: rds.DatabaseInstance;
+  public readonly dbSecret: secretsmanager.ISecret;
+  public readonly appSecret: secretsmanager.ISecret;
+  public readonly dbHost: string;
+
+  constructor(scope: Construct, id: string, props: RdsStackProps) {
+    super(scope, id, props);
+
+    // 4. シークレット作成（admin）
+    this.dbSecret = new rds.DatabaseSecret(this, 'AdminDbSecret', {
+      secretName: 'admin-db-credentials',
+      username: 'admin',
+    });
+
+    // 5. シークレット作成（app_user）
+    const appSecret = new secretsmanager.Secret(this, 'CustomerInfoAppSecret', {
+      secretName: 'customer-info-app-credentials',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'app_user' }),
+        generateStringKey: 'password',
+        excludePunctuation: true,
+      },
+    });
+
+    this.appSecret = appSecret;
+
+    // 6. RDS MySQLインスタンス作成
+    this.dbInstance = new rds.DatabaseInstance(this, 'ApplicationDb', {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_42,
+      }),
+      instanceIdentifier: 'application-db',      // ★RDS インスタンス名
+      vpc: props.vpc,
+      vpcSubnets: { subnetGroupName: 'db-private' },
+      instanceType: new ec2.InstanceType(process.env.DB_INSTANCE_TYPE ?? 't3.micro'),
+      securityGroups: [props.dbSg],
+      credentials: rds.Credentials.fromSecret(this.dbSecret),
+      multiAz: false,
+      storageType: rds.StorageType.GP3,
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      deletionProtection: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      databaseName: 'customer_info',              // ★初期DB名
+    });
+
+    this.dbHost = this.dbInstance.dbInstanceEndpointAddress;
+    
+    // 7. 出力
+    new cdk.CfnOutput(this, 'ApplicationDbEndpoint', {
+      value: this.dbInstance.dbInstanceEndpointAddress,
+    });
+    new cdk.CfnOutput(this, 'AdminDbSecretName', {
+      value: this.dbSecret.secretName,
+    });
+    new cdk.CfnOutput(this, 'AppSecretName', {
+      value: this.appSecret.secretName,
+    });
+    new cdk.CfnOutput(this, 'AppSecretArn', {
+      value: this.appSecret.secretArn,
+    });
+  }
+}
