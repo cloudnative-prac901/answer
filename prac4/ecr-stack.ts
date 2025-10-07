1850932ebfe44df3a84cab5ec0941d9ac2a1de00
cloudnative-prac901 <cloud.native.practice.25@gmail.com>
2025-10-07 10:15:37 +0900

    Update ecr-stack.ts
    add fortune-telling/app repository

--- prac4/ecr-stack.ts
+++ prac4/ecr-stack.ts
@@ -5,31 +5,48 @@ import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 // 2. スタック初期化
 export class EcrStack extends cdk.Stack {
-  public readonly repository: ecr.Repository;
+  // public readonly repository: ecr.Repository;  ★コメントアウト
+  public readonly customerInfoRepo: ecr.Repository;    // ★顧客情報表示機能用
+  public readonly fortuneTellingRepo: ecr.Repository;  // ★占い機能用
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // 3. リポジトリ作成
-    this.repository = new ecr.Repository(this,'CustomerInfoAppRepo',{
-        repositoryName : 'customer-info/app', // 作成するリポジトリ名
-        imageScanOnPush: true,                // 自動スキャン
-        removalPolicy  : cdk.RemovalPolicy.RETAIN, // 誤削除防止
-      },
-    );
+    // 3. CustomerInfoApp リポジトリ作成
+    this.customerInfoRepo = new ecr.Repository(this, 'CustomerInfoAppRepo', {  // ★変数名を修正
+      repositoryName : 'customer-info/app',       // 作成するリポジトリ名
+      imageScanOnPush: true,                      // 自動スキャン
+      removalPolicy  : cdk.RemovalPolicy.RETAIN,  // 誤削除防止
+    });
 
-    // 4. ライフサイクル
-    this.repository.addLifecycleRule({
+    // 4. FortuneTellingApp リポジトリ作成　★セクション追加
+    this.fortuneTellingRepo = new ecr.Repository(this, 'FortuneTellingAppRepo', {
+      repositoryName : 'fortune-telling/app',
+      imageScanOnPush: true,
+      removalPolicy  : cdk.RemovalPolicy.RETAIN,
+    });
+
+    // 5. ライフサイクルルール
+    // ★2リポジトリにまとめて設定するためにコードを修正
+    const lifecycleRule = {
       description : 'Delete images older than 7 days',
       maxImageAge : cdk.Duration.days(7),
       tagStatus   : ecr.TagStatus.ANY,
-    });
+    };
+    this.customerInfoRepo.addLifecycleRule(lifecycleRule);    // 既存アプリ
+    this.fortuneTellingRepo.addLifecycleRule(lifecycleRule);  // 新規アプリ
 
-    // 5. 出力
-    new cdk.CfnOutput(this, 'CustomerInfoAppRepoUri',{
-      value      : this.repository.repositoryUri,
+    // 6. 出力
+    new cdk.CfnOutput(this, 'CustomerInfoAppRepoUri', {  // ★変数名を修正
+      value      : this.customerInfoRepo.repositoryUri,
       description: 'ECR repository URI for customer-info/app',
       exportName : 'customerInfoAppRepoUri',
+    });
+    // ★新規アプリケーションの出力設定
+    new cdk.CfnOutput(this, 'FortuneTellingAppRepoUri', {
+      value      : this.fortuneTellingRepo.repositoryUri,
+      description: 'ECR repository URI for fortune-telling/app',
+      exportName : 'fortuneTellingAppRepoUri',
     });
   }
 }
